b0869cbf0ad269c408748e1120a61e4dacbaba9b
Quitiweb <rafa@quitiweb.com>
2019-11-04 15:05:58 +0100

    Cambios en las urls para que, al recuperar users, no haya que incluir previamente la referencia /log/ para evitar confusiones.
     - Ahora solo hay que inlucir /users/ para obtener el listado de los usuarios
     - /log/ sigue funcionando igual

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.LogDetail.as_view(), name='log-detail'),
+    path('log/', views.LogDetail.as_view(), name='log-detail'),
     path('users/', views.UserList.as_view(), name='user-list'),
 ]
 
