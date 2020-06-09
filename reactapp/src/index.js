d5865eb72e37371cbd770f64867900e31146f9fb
Quitiweb <rafa@quitiweb.com>
2020-06-09 17:59:18 +0200

    url from http to https

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -33,7 +33,7 @@ const memoryHistory = createMemoryHistory();
 // const hist = createBrowserHistory();
 
 // window.$BASE_URL = 'http://127.0.0.1:8000';
-window.$BASE_URL = 'http://api.quitiweb.com';
+window.$BASE_URL = 'https://api.quitiweb.com';
 
 ReactDOM.render(
   <Router history={memoryHistory}>
